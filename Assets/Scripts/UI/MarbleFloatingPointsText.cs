32042a3a57e8f5fb1a04e9e22b10451e0986d90a
Josh Klope <klope3@msn.com>
2026-01-02 11:00:46 -0600

    Add floating text for points

---
+++ Assets/Scripts/UI/MarbleFloatingPointsText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+
+public class MarbleFloatingPointsText : MonoBehaviour
+{
+    [SerializeField] private Marble marble;
+    [SerializeField] private MMF_Player floatingTextPlayer;
+
+    private void Awake()
+    {
+        marble.OnEarnPoints += Marble_OnEarnPoints;
+    }
+
+    private void Marble_OnEarnPoints(int points)
+    {
+        MMF_FloatingText floatingText = floatingTextPlayer.GetFeedbackOfType<MMF_FloatingText>();
+        floatingText.Value = $"+${points}";
+        floatingTextPlayer.PlayFeedbacks();
+    }
+}
